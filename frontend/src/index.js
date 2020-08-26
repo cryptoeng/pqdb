1e2140968037f4e5fef60938b5ea0dde555b18f3
Marian Sigler <sigler@inf.fu-berlin.de>
2020-08-26 19:38:36 +0200

    First (unsuccessful) try to scroll up after page change
    Following https://reactrouter.com/web/guides/scroll-restoration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useEffect } from "react";
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+import { BrowserRouter as Router, withRouter, useLocation } from 'react-router-dom';
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 const AppWithRouter = withRouter(props => <App {...props} />);
 ReactDOM.render(
   <React.StrictMode>
     <Router basename={"/pqdb"}>
+      <ScrollToTop />
       <AppWithRouter />
     </Router>
   </React.StrictMode>,
